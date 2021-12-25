5058556628cb7aa6e5b18ca7d623bca5c249c53b
Abiodun Samuel <abiodunsamyemi@gmail.com>
2021-12-25 04:01:17 +0100

    first deploy

---
+++ index.js
@@ -0,0 +1,31 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import router from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/posts", router);
+app.get("/", (req, res) => {
+  res.send("Hello, this is my Memories API");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //  useFindAndModify: false,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server is runnning on Port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+// mongoose.set("useFindAndModify", false);
